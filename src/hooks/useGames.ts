e04cdc17735ba10a240c5ad9753f2d8799880d94
Destiny Ayomah <ayomahdestiny@gmail.com>
2023-04-29 00:37:16 +0100

    Created Game Cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
     id: number,
-    name: string
+    name: string,
+    background_image: string
 }
 
 interface FetchedGameResponse {
@@ -23,7 +24,7 @@ const useGames = () => {
             .then((res) => setGames(res.data.results))
             .catch(err => {
                 if (err instanceof CanceledError) return;
-                    setError(err.message)
+                setError(err.message)
             });
 
         return () => controller.abort();
