acb916b60e1d75eac6f97a2c93d2526d9ec04446
Destiny Ayomah <ayomahdestiny@gmail.com>
2023-04-29 02:55:03 +0100

    Refactored Duplicate styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,8 +1,8 @@
-import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react"
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
     return (
-        <Card overflow={"hidden"} width='300px' >
+        <Card>
             <Skeleton height='200px' />
             <CardBody>
                 <SkeletonText />
