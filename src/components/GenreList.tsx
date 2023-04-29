6eada3ecae6fba8148bbe41fb11d686bf91a0313
Destiny Ayomah <ayomahdestiny@gmail.com>
2023-04-29 13:43:16 +0100

    Displaying thes genre with better styling

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,21 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenres"
+import getCroppedImageUrl from "../services/get-cropped-image-url";
 
 const GenreList = () => {
     const { data } = useGenre();
 
     return (
-        <ul>
-            {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
+        <List>
+            {data.map(genre =>
+                <ListItem key={genre.id} paddingY='10px'>
+                    <HStack>
+                        <Image boxSize='32px' src={getCroppedImageUrl(genre.image_background)} borderRadius={3} />
+                        <Text fontSize='lg'>{genre.name}</Text>
+                    </HStack>
+                </ListItem>
+            )}
+        </List>
     )
 }
 
